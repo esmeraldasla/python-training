1e174b1a3d9cbec5fbf684e40732951aa0391895
esmeraldalima <mel_limaa6@hotmail.com>
2022-07-09 16:05:16 -0300

    First version

---
+++ forloop_pizza.py
@@ -0,0 +1,13 @@
+#09/07/2022 This is to train the for loop.
+
+pizzas = ['chicken and catupity', 'corn', 'chicken and cheese']
+for pizza in pizzas:
+ print(pizza)
+
+#Modify your for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza. For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
+
+pizzas = ['chicken and catupity', 'corn', 'chicken and cheese']
+for pizza in pizzas:
+ print(f"I like {pizza} pizza! \n")
+
+print("I didn't like corn pizza before, but now it is one of my favorites. I also like chicken and cheese as well as chicken and catupity. I really love pizza!")
